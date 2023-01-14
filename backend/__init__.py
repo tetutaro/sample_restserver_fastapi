5a97b2f8637f25e395edd89b2f28a61a913f2ea0
Tetsutaro Maruyama <tetsutaro.maruyama@gmail.com>
2023-01-14 23:57:23 +0900

    v0.3.1

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-__version__ = "0.3.0"  # automatically updated by poetry-dynamic-versioning
+__version__ = "0.3.1"  # automatically updated by poetry-dynamic-versioning
